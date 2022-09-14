262dfc80c949692142b184937e2b38c6f1103a66
Sneik <Sneik92@ya.ru>
2022-09-14 16:35:54 +0300

    После отправки формы из модалки сначала скроется модалка а после появится сообщение спасибо

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,11 +2,14 @@ document.addEventListener('DOMContentLoaded', (e) => {
 	
   document.addEventListener('wpcf7mailsent', function (response) {
 	 const modalThanks = document.querySelector('#modal-form-thanks');
+   const mainModal = document.querySelector('#modal-form');
+   
 		if (modalThanks) {
 			  modalThanks.classList.add("modal-form-thanks__visible");
 			  const modalThanksCloseBtn = modalThanks.querySelector('.modal-form-thanks__close');
 			  modalThanksCloseBtn.addEventListener("click", closeModal);
 		}
+  if (mainModal) mainModal.classList.remove('modal-form__visible');
 	 function closeModal() {
       	modalThanks.classList.remove("modal-form-thanks__visible");
     }
