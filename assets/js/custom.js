a7d6df8db6823a44a4e9de129416e49402258e33
Sneik <Sneik92@ya.ru>
2022-09-14 16:19:02 +0300

    Добавил в custom.js обработку успешной отправки CF7

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,15 @@
-// document.addEventListener('DOMContentLoaded', (e) => {
-//   const f1 = document.querySelector('#documents-and-sertificates-type');
-//   const f2 = document.querySelector('#documents-and-sertificates-amount form');
-//   const f1Button = document.querySelector('#documents-and-sertificates-type .custom-select__btn');
-//   const f2Button = document.querySelector('#documents-and-sertificates-amount .custom-select__btn');
-// console.log(f1)
-// console.log(f2)
-// console.log(f1Button)
-// console.log(f2Button)
-//   f1.addEventListener('submit', (e) => {
-    
-//       f2.submit();
- 
-// })
-// })
+document.addEventListener('DOMContentLoaded', (e) => {
+	
+  document.addEventListener('wpcf7mailsent', function (response) {
+	 const modalThanks = document.querySelector('#modal-form-thanks');
+		if (modalThanks) {
+			  modalThanks.classList.add("modal-form-thanks__visible");
+			  const modalThanksCloseBtn = modalThanks.querySelector('.modal-form-thanks__close');
+			  modalThanksCloseBtn.addEventListener("click", closeModal);
+		}
+	 function closeModal() {
+      	modalThanks.classList.remove("modal-form-thanks__visible");
+    }
+  });
+	
+});
