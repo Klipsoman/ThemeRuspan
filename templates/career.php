e1b2c187e1ac1519a9e8da5eaabe8512e473c033
Sneik <Sneik92@ya.ru>
2022-10-13 14:13:39 +0300

    Added vidget of company

--- templates/career.php
+++ templates/career.php
@@ -87,9 +87,7 @@
                 Ruspan
                 в России на сайте hh.ru.</p>
               <div class="career-feature__hh">
-                <script class="hh-script"
-                  src="https://api.hh.ru/widgets/vacancies/search?count=3&locale=RU&links_color=1560b2&border_color=1560b2&currency=AZN&only_with_salary=false&salary=100000&text=%D0%BE%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80+%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D0%B3%D0%BE+%D1%83%D1%87%D0%B0%D1%81%D1%82%D0%BA%D0%B0">
-                </script>
+                <script class="hh-script" src="https://api.hh.ru/widgets/vacancies/employer?employer_id=246062&locale=RU&links_color=1560b2&border_color=1560b2"></script>
               </div>
             </div>
             <div class="career-feature__item">
