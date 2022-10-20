81e77ddeb9ca68e9225112c485d00061203472dc
Sneik <Sneik92@ya.ru>
2022-10-20 17:56:10 +0300

    страница проектирования теперь текст можно менять с админки

--- templates/design.php
+++ templates/design.php
@@ -24,11 +24,8 @@
           </div>
         </div>
         <div class="page-photo-description__right">
-          <p class="description">Ruspan осуществляет проектирование быстровозводимых зданий и сооружений на основе
-            стальных конструкций, используя современные системы автоматизированного проектирования, а также
-            информационного моделирования.</p>
-          <p class="description-secondary">На сегодняшний день реализовано более 1000 объектов в коммерческой
-            и промышленной сферах.</p>
+          <p class="description"><?= CFS()->get('design_main-text') ?></p>
+          <p class="description-secondary"><?= CFS()->get('design_sub-text') ?></p>
         </div>
       </div>
     </div>
@@ -38,22 +35,16 @@
     <div class="container">
       <div class="abouts__content">
         <div class="abouts__item">
-          <h3 class="abouts__title description">Комплексное сопровождение проекта</h3>
-          <p class="abouts__descr description-secondary">Проектный отдел компании осуществляет авторский надзор,
-            шеф-монтаж и полное сопровождение проекта в процессе строительно-монтажных работ, а также при прохождении
-            экспертизы.</p>
+          <h3 class="abouts__title description"><?= CFS()->get('design_block1-header') ?></h3>
+          <p class="abouts__descr description-secondary"><?= CFS()->get('design_block1-text') ?></p>
         </div>
         <div class="abouts__item">
-          <h3 class="abouts__title description">BIM-технологии в разработке проектов зданий</h3>
-          <p class="abouts__descr description-secondary">Для создания 3D-моделей будущего здания специалисты Ruspan
-            разрабатывают проекты зданий комплексно в программах AutoCAD, SolidWorks, Tekla Structures, SCAD и Revit.
-          </p>
+          <h3 class="abouts__title description"><?= CFS()->get('design_block2-header') ?></h3>
+          <p class="abouts__descr description-secondary"><?= CFS()->get('design_block2-text') ?></p>
         </div>
         <div class="abouts__item">
-          <h3 class="abouts__title description">Разработка инновационных продуктов</h3>
-          <p class="abouts__descr description-secondary">Одна из приоритетных задач Ruspan — разработка и создание новых
-            инновационных продуктов. Специалисты компании тесно сотрудничают с ведущими научно-исследовательскими
-            институтами России.</p>
+          <h3 class="abouts__title description"><?= CFS()->get('design_block3-header') ?></h3>
+          <p class="abouts__descr description-secondary"><?= CFS()->get('design_block3-text') ?></p>
         </div>
       </div>
     </div>
