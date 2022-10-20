c2f8431c3739eb81e3552fcc791f614187bc2409
Sneik <Sneik92@ya.ru>
2022-10-20 16:10:13 +0300

    страница карьера. добавлена возможность изменять контент

--- templates/career.php
+++ templates/career.php
@@ -19,17 +19,11 @@
     <div class="career-inner">
       <div class="container">
         <div class="career-inner__content">
-          <div class="career-inner__left">
-            <p class="career-inner__descr description">Компания Ruspan (ранее Kingspan) является ведущим производителем
-              сэндвич-панелей и каркасов для быстровозводимых зданий, а также оказывает проектную поддержку для нежилого
-              строительства на всех стадиях от проектирования до сдачи в эксплуатацию. Мы создаем высокоэффективные
-              здания
-              как готовые решения для наших клиентов. Продукция компании представлена в России и странах СНГ.</p>
+          <div class="career-inner__left">	
+            <p class="career-inner__descr description"><?= CFS()->get('career_blue-text-left') ?></p>
           </div>
           <div class="career-inner__right">
-            <p class="career-inner__descr description-secondary">Компания представлена двумя юридическими лицами:
-              ООО «Кингспан» и ООО «Кингспан Невинномысск». Головной офис находится в Санкт-Петербурге, а 2 завода
-              в г. Гатчине Ленинградской области и г. Невинномысске Ставропольского края.</p>
+            <p class="career-inner__descr description-secondary"><?= CFS()->get('career_blue-text-right') ?></p>
           </div>
         </div>
       </div>
@@ -40,25 +34,25 @@
       <div class="container">
         <div class="stats__grid">
           <div class="stats__area">
-            <div class="stats__number">>750</div>
+            <div class="stats__number"><?= CFS()->get('career_values1') ?></div>
             <div class="stats__descr description-secondary">
-              строительных проектов в России и СНГ в год
+             <?= CFS()->get('career_values1-text') ?>
             </div>
           </div>
           <div class="stats__area">
-            <div class="stats__number">2</div>
+            <div class="stats__number"><?= CFS()->get('career_values2') ?></div>
             <div class="stats__descr description-secondary">
-              производственные площадки
+            <?= CFS()->get('career_values2-text') ?>
             </div>
           </div>
           <div class="stats__area">
-            <div class="stats__number">180</div>
-            <div class="stats__descr description-secondary">сотрудников</div>
+            <div class="stats__number"><?= CFS()->get('career_values3') ?></div>
+            <div class="stats__descr description-secondary"><?= CFS()->get('career_values3-text') ?></div>
           </div>
           <div class="stats__area">
-            <div class="stats__number">>10</div>
+            <div class="stats__number"><?= CFS()->get('career_values4') ?></div>
             <div class="stats__descr description-secondary">
-              лет работы в России
+             <?= CFS()->get('career_values4-text') ?>
             </div>
           </div>
         </div>
@@ -69,30 +63,22 @@
     <div class="career-feature">
       <div class="container">
         <div class="career-feature__content">
-          <h2 class="career-feature__title page-title">Наша экспертная команда — одна из наших ключевых ценностей</h2>
+          <h2 class="career-feature__title page-title"><?= CFS()->get('career_widget-header') ?></h2>
           <div class="career-feature__grid">
             <div class="career-feature__item">
-              <p class="career-feature__descr description-secondary">Мы успешно работаем в отрасли с 2008 г. в России
-                и с 1965 г. в мире. Наша команда переняла лучшее из европейского и российского опыта, мы реализуем более
-                750 проектов в год на территории России и СНГ. Ruspan отбирает лучшие кадры на рынке, а наши
-                сотрудники —
-                это профессионалы с многолетним опытом работы в строительной отрасли.</p>
+              <p class="career-feature__descr description-secondary"><?= CFS()->get('career_text-block1') ?></p>
             </div>
             <div class="career-feature__item">
-              <p class="career-feature__descr description-secondary">Мы всегда заинтересованы в привлечении
-                квалифицированных, предприимчивых людей, которые могли бы помочь в развитии нашего бизнеса.</p>
+              <p class="career-feature__descr description-secondary"><?= CFS()->get('career_text-block2') ?></p>
             </div>
             <div class="career-feature__item">
-              <p class="career-feature__descr description-secondary">Ознакомьтесь с актуальными вакансиями компании
-                Ruspan
-                в России на сайте hh.ru.</p>
+              <p class="career-feature__descr description-secondary"><?= CFS()->get('career_text-block3') ?></p>
               <div class="career-feature__hh">
                 <script class="hh-script" src="https://api.hh.ru/widgets/vacancies/employer?employer_id=246062&locale=RU&links_color=1560b2&border_color=1560b2"></script>
               </div>
             </div>
             <div class="career-feature__item">
-              <p class="career-feature__descr description-secondary">Если вы не увидели подходящей вакансии, вы можете
-                отправить своё резюме с сопроводительным письмом на адрес <a class="career-feature__mail"
+              <p class="career-feature__descr description-secondary"><?= CFS()->get('career_text-block4') ?> <a class="career-feature__mail"
                   href="mailto:hr@ruspan.su">hr@ruspan.su.</a></p>
             </div>
           </div>
